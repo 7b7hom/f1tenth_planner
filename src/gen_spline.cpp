fe504ae2707ba003884058a0eb2090d0df630736
7b7hom <cherishu1012@gmail.com>
2025-07-08 00:33:34 +0900

    gen_spline() - scaling

--- src/gen_spline.cpp
+++ src/gen_spline.cpp
@@ -9,6 +9,16 @@ struct SplineResult {
     MatrixXd normvec_normalized;// 각 구간의 법선 벡터를 정규화한 값 (구간 개수 x 2)
 };
 
+// 경로 행렬의 각 점 사이의 유클리드 거리 계산 함수
+VectorXd computeEuclideanDistances(const MatrixXd& path) {
+    int N = path.rows() - 1;    // 구간 개수는 점 개수 - 1
+    VectorXd dists(N);          // 거리 저장 벡터
+    for (int i = 0; i < N; ++i) {
+        dists(i) = (path.row(i + 1) - path.row(i)).norm();  // 두 점 사이 거리 계산 (유클리드 노름)
+    }
+    return dists;
+}
+
 // path : x,y 좌표 배열
 // el_lengths 전달 안 하면 nullptr (점 사이 거리는 필수 x)
 // psi_s, psi_e도 전달 안 하면 NAN (필수 x)
@@ -16,20 +26,40 @@ SplineResult calcSplines(const MatrixXd& path, const VectorXd* el_lengths = null
                          double psi_s = NAN, double psi_e = NAN,
                          bool use_dist_scaling = true) {
 
-        // 폐곡선 여부 판단
+        // 1.폐곡선 여부 판단
         MatrixXd updated_path = path;
-
-        bool closed = (path.row(0) - path.row(path.rows() - 1)).norm() < 1e-6 && std::isnan(psi_s);
+        
+        // 시작 점과 끝 점 사이 거리가 매우 작으면 같다고 간주한다.
+        bool closed = (path.row(0) - path.row(path.rows() - 1)).norm() < 1e-6;
 
         if (closed) {
-            // 폐곡선 유지: 시작점 다시 한 번 넣기
+            // 폐곡선 유지 : 시작점 다시 한 번 넣기 (마지막 스플라인 조각을 만들기 위해서)
             updated_path.conservativeResize(path.rows() + 1, Eigen::NoChange);
             updated_path.row(updated_path.rows() - 1) = path.row(0);
         }
 
-        int no_splines = updated_path.rows() - 1;
+        int no_splines = updated_path.rows() - 1;   // 경로 구간 수 = 점 개수 - 1
 
+        VectorXd ds;
+        // 유클리드 거리 담고 있는 el_lengths (nullptr -> 유클리드 거리 계산, 아니면 el_lengths가 가리키는 VerctorXd 객체 거리 사용)
+        if (el_lengths == nullptr) {
+            ds = computeEuclideanDistances(updated_path);  // 기본 거리 계산 함수 호출
+        } else {
+            ds = *el_lengths;  // 외부에서 받은 길이 벡터 사용
+        }
 
+        // 2. 스케일링
+        // ds라는 거리 벡터가 계산된 상태에서, 각 구간 간의 길이 비율(scaling)을 계산 해 VectorXd scaling에 저장
+        // scaling 벡터 크기 : no_splines - 1 (두 스플라인 사이의 연결점 개수 == 스플라인 개수 - 1)
+
+        // 이후 스플라인 연립방정식 구성 시 사용 (연속성 조건에 반영)
+        VectorXd scaling = VectorXd::Ones(no_splines - 1);  // 기본값: 모두 1.0
+        if (use_dist_scaling) {
+            for (int i = 0; i < no_splines - 1; ++i) {
+                scaling(i) = ds(i) / ds(i + 1);  // 인접 구간 간의 거리 비율 계산
+            }
+        }
+    
 
 }
 
